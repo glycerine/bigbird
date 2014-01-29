1a452c02f8ee767acf50f6a5f86fcc137c847f1e
Jason E. Aten <j.e.aten@gmail.com>
2014-01-29 13:21:11 -0800

    repl.go has stubbed out three phases of processing: grab a line of golang input, translate into scheme, pass to scheme evaluator

--- repl.go
+++ repl.go
@@ -1,30 +1,74 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
-	linenoise "github.com/GeertJohan/go.linenoise"
+	"go/parser"
+	"go/token"
 	"github.com/glycerine/bigbird/gochick"
 )
 
 func main() {
 	gochick.Start()
 
+	bParsing := false
+	scanner := bufio.NewScanner(os.Stdin)
+
+	lineNumber := 0
+	line := ""
+	var ok bool
+
+	wrapper := `
+     package main
+     import (
+       "fmt"
+     )
+     func main() {
+      maincode := 0
+     %s
+     fmt.Printf("\n :maincode = %%d.\n", maincode)
+     }
+`
+
 	for {
-		line, err := linenoise.Line("> ")
-		if err != nil {
-			if err.Error() == "prompt was quited with a killsignal" {
-				//fmt.Printf("ignoring: prompt was quited with a killsignal\n")
-				continue
-			}
-			fmt.Printf("error: %v\n", err)
+		lineNumber++
+		fmt.Printf("%04d > ", lineNumber)
+		ok = scanner.Scan()
+		if !ok {
+			fmt.Printf("\nok was false. quitting.\n")
+			os.Exit(0)
+		}
+
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+		line = scanner.Text()
+
+		if line == ".parseoff" {
+			bParsing = false
+			fmt.Printf("parsing as golang off.\n")
 			continue
 		}
-		//fmt.Printf("line is %v\n", line)
 
-		/*
+		if line == ".parseon" {
+			bParsing = true
+			fmt.Printf("parsing as golang on.\n")
+			continue
+		}
+
+		fmt.Printf("line is: '%s'\n", line)
+
+		wrapped := fmt.Sprintf(wrapper, line)
+		fmt.Printf("wrapped line is: '%s'\n", wrapped)
+
+		if bParsing {
+			// first step of translating from golang to scheme:
+			//  parse the go code using the standard libraries
+			//  token and parse
 			fset := token.NewFileSet()
-			f, err := parser.ParseFile(fset, "", interface{}(line), parser.Trace)
+			f, err := parser.ParseFile(fset, "", interface{}(wrapped), parser.Trace)
 			if err != nil {
 				fmt.Printf("parse error: %v\n", err)
 				continue
@@ -32,12 +76,15 @@ func main() {
 			if f == nil {
 				fmt.Printf("ast returned was nil\n")
 			} else {
-				fmt.Printf("f is %v\n", f)
+				fmt.Printf("ast in f is %v\n", f)
 			}
-		*/
+		}
+
+		// step two: translate from golang to scheme
 		schemeSrc := line
 
+		// step three: evaluate in the chicken-scheme repl
 		s := gochick.Eval(schemeSrc)
-		fmt.Printf("%v\n", s)
+		fmt.Printf("chicken-scheme eval result: '%s'\n", s)
 	}
 }
